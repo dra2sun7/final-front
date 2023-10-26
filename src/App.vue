f02e1dfc826b932cdbbe07cceb5e008c2be1a10d
dra2sun7 <dra2sun7@gmail.com>
2023-10-26 18:52:08 +0900

    1026 last commit

---
+++ src/App.vue
@@ -0,0 +1,162 @@
+
+<template>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <div class="black-bg" v-if="isLoading==true"><div class="container">
+  <div class="spinner" style="margin-top: 250px;"></div>
+  </div></div>
+
+
+    <div class="black-bg" v-if="flag==true">
+    <div class="container"><div class="text-box">
+      <p v-for="message in logMessage" :key="message" v-html="formatLogMessage(message)"></p>
+    </div></div><button type="button" class="btn btn-primary" style="margin-top: 10px;" @click="flag=false">닫기</button></div>
+    
+  <div>
+    <router-link to="/" class="header-link">
+    <h1>KCS2</h1>
+    <h4>Kubernetes Client Scanner Service</h4>
+    </router-link>
+  </div>
+  <div>
+    <h4 style="margin-right: 5px; margin-top: 100px;">URL</h4>
+    <p><input type="text" style="width: 30%; height: 30px; font-size: 15px;" v-model="apiServer" autocomplete="off" placeholder="URL을 입력하세요"></p>
+    <h4 style="margin-right: 5px;">Token</h4>
+    <p><input type= "password" style="width: 30%; height: 30px; font-size: 15px;" v-model="token" placeholder="Token을 입력하세요"></p>
+    <button type="button" class="btn btn-primary" @click="sendDataToBackend">Primary</button>
+
+  </div>
+</template>
+
+<script>
+//import HomeButton from './components/HomeButton.vue'; // Import the HomeButton component
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      inputText : "각각의 NODE에 대한 LOG",
+      logMessage : [],
+      flag : false,
+      isLoading : false,
+      apiServer : '',
+      token : '',
+    }
+  },
+  methods: {
+    formatLogMessage(rawLog) {
+      // Process the log message here, such as adding line breaks or other formatting
+      const formattedLog = rawLog.replace(/\n/g, '<br>'); // Replace line breaks with <br> tags
+      return formattedLog;
+    },
+    sendDataToBackend() {
+        // Data to send to the backend
+      this.isLoading = true;
+      const data = { apiServer: this.apiServer, token: this.token };
+      axios.post('http://localhost:8080/api/runCurl', data)
+           .then(response => {
+          // Handle the response from the backend
+              this.logMessage = response.data;
+           })
+           .catch(error => {
+            // Handle any errors
+              console.error(error);
+            })
+            .finally(() => {
+              this.isLoading = false;
+              this.flag = true;
+            });
+    },
+  },  
+  components: {
+    //'home-button': HomeButton // Register the HomeButton 
+  },
+}
+</script>
+
+<style>
+body{
+  margin : 0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg{
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg{
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.room-img{
+  width: 30%;
+  margin-top: 40px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+.text-box {
+  border: 1px solid #ccc; /* Add a border */
+  padding: 10px; /* Add some padding */
+  background-color: #f0f0f0; /* Set a background color */
+  border-radius: 5px; /* Add rounded corners */
+  margin-top: 10px; /* Adjust margin for spacing */
+
+  width: 1300px;
+  height: 600px;
+  
+  overflow-y: scroll;
+  overflow-x: hidden;
+
+  text-align: left;
+  white-space: pre-line;
+}
+.container {
+  display: flex;
+  justify-content: center; /* Center horizontally */
+  align-items: center; /* Center vertically */
+  /* height: 50vh;  Optional: Make the container full height of the viewport */
+}
+.header-link h1,
+.header-link h4 {
+  display: inline;
+  margin-right: 15px;
+}
+.spinner {
+    border: 8px solid rgba(0, 0, 0, 0.1);
+    border-left: 8px solid #3498db;
+    border-radius: 100%;
+    width: 80px;
+    height: 80px;
+    animation: spin 2s linear infinite;
+  }
+
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+a {
+  color: inherit; /* Use the default text color */
+  text-decoration: none; /* Remove underline */
+}
+</style>
