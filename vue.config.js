23b23ebff1569835a7d4b1cf06b09641ae64b067
dra2sun7 <dra2sun7@naver.com>
2023-10-30 21:25:06 +0900

    version 1.2 solution about Invalid Host header

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,12 @@
 module.exports = {
   transpileDependencies: ['vue'],
   
-  devServer: {
-    allowedHosts: "all",
-  },
   // npm run build 타겟 디렉토리 -> 해당 경로에 Vue 빌드 출력물 생성됨
   outputDir: '../backend/src/main/resources/static',
 
   // npm run serve 개발 진행시에 포트가 다르기 때문에 프록시 설정
   devServer: {
+    allowedHosts: "all",
     proxy: 'http://kcs2.co.kr:8080' // 서버 프로젝트 포트번호와 동일할 것
   },
 };
